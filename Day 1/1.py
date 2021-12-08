f5e59933f2020f3ae5544a52cc91cc5328e49833
WiseNat <nathan88wise@gmail.com>
2021-12-08 12:45:41 +0000

    Day 1 Solution

--- Day 1/1.py
+++ Day 1/1.py
@@ -1,2 +1,10 @@
 if __name__ == "__main__":
-    pass
+    with open("input.txt") as f:
+        counter = 0
+        data = [int(i) for i in f.read().split("\n")]
+        for i in range(len(data) - 1):
+            if data[i] < data[i + 1]:
+                counter += 1
+
+    print(f"Result: {counter}")
+
