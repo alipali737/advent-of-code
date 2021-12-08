78d6b75d3a346a7d3f9102229306bd476427f438
Alipali <44681565+alipali737@users.noreply.github.com>
2021-12-08 15:59:20 +0000

    Update template.py

--- template.py
+++ template.py
@@ -1,8 +1,12 @@
 from pydoc import importfile
 f = importfile("../functions.py")
 
-def main():
+def Step1():
     # code
 
+def Step2():
+    # code
+    
 if __name__ == "__main__":
-    f.average_time(main)
+    f.average_time(Step1)
+    f.average_time(Step2)
