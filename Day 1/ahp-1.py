0484ea0de62f098e86c71498639c8e0f4e7bce76
Alec Painter <Alec.Painter@ibm.com>
2021-12-08 15:35:49 +0000

    Finished for step 2

--- Day 1/ahp-1.py
+++ Day 1/ahp-1.py
@@ -1,7 +1,7 @@
-with open("input.txt") as f:
+with open("./Day 1/input.txt") as f:
     lines = [int(line.rstrip()) for line in f.readlines()]
     counter = 0
-    for i in range(len(lines)-1):
-            if lines[i] < lines[i+1]:
-                counter+=1
+    for i in range(len(lines)-3):
+        if sum(lines[i:i+3]) < sum(lines[i+1:i+4]):
+            counter+=1
     print(counter)
