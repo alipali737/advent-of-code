ca819fe8e576391b1b707de94eb4133cd35afce0
WiseNat <nathan88wise@gmail.com>
2021-12-20 08:46:15 +0000

    Day 5 Part 2

--- Day-5/wisenat-5.py
+++ Day-5/wisenat-5.py
@@ -6,17 +6,25 @@ f = importfile("../functions.py")
 
 def printGrid(grid):
     for row in grid:
-        print(" ".join(map(str, row)))
+        print(" ".join([i.replace("0", ".") for i in map(str, row)]))
 
 def gridToCsv(grid):
-    df = pd.DataFrame(numpy.array(grid).transpose())
+    df = pd.DataFrame(numpy.array(grid))
     df.to_csv("grid.csv", index=False)
 
 def generateBoundingGrid(coords):
     bounding = numpy.max(coords, 0)
     return [[0 for x in range(bounding[0] + 1)] for y in range(bounding[1] + 1)]
 
-def addLines(grid, lines):
+def arange(start, stop, inc=1):
+    if start > stop:
+        inc *= -1
+        stop -= 1
+    else:
+        stop += 1
+    return range(start, stop, inc)
+
+def addLines(grid, lines, diagonal=False):
     # 1 Iteration = 1 Line
     for head, tail in lines:
         def genIterSel(c1, c2, val):
@@ -33,8 +41,11 @@ def addLines(grid, lines):
         elif head[1] == tail[1]:
             x, y = genIterSel(head, tail, 1)
         else:
-            # print("Not a straight line..")
-            continue
+            if diagonal:
+                x = arange(head[0], tail[0])
+                y = arange(head[1], tail[1])
+            else:
+                continue
         
         for x, y in zip(*[x, y]):
             grid[y][x] += 1
@@ -46,7 +57,8 @@ def part1():
     1. Read in all the data in a nice format: [[head_coord1, tail_coord1], [head_coord2, tail_coord2], ...]
     2. Find biggest value for X and Y in data, generate an empty grid using these values
     3. Iterate through data, appending lines to the grid
-    4. Count values of 0 and 1 and subtract them from the length*width of the grid'''
+    4. Count values of 0 and 1 and subtract them from the length*width of the grid
+    '''
 
     with open("input.txt") as f:
         lines = numpy.array([[list(map(int, coords.split(","))) for coords in line.split(" -> ")] for line in f.read().split("\n")])
@@ -62,12 +74,27 @@ def part1():
     
 
 def part2():
-    return None
+    '''
+    Same as part 1 but we pass a diagonal=True argument to addLines
+    '''
+
+    with open("input.txt") as f:
+        lines = numpy.array([[list(map(int, coords.split(","))) for coords in line.split(" -> ")] for line in f.read().split("\n")])
+    
+    coords = lines.reshape(-1, lines.shape[-1])
+    grid = generateBoundingGrid(coords)
+    grid = addLines(grid, lines, diagonal=True)
+    # gridToCsv(grid)
+    # printGrid(grid)
+    
+    result = len(grid) * len(grid[0]) - sum(row.count(0) + row.count(1) for row in grid)
+    
+    return result
 
 
 if __name__ == "__main__":
     s1 = f.average_time(part1, iter=1)
-    # s2 = f.average_time(part2)
+    s2 = f.average_time(part2, iter=1)
     
     print(f"Part 1 - Average Time: {s1[0]}, Result: {s1[1]}")
-    # print(f"Part 2 - Average Time: {s2[0]}, Result: {s2[1]}")
+    print(f"Part 2 - Average Time: {s2[0]}, Result: {s2[1]}")
